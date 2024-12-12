cb7bdf16bda1b9da5f5c27491b2c54329ac1fc63
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-12 10:20:26 +0100

    Add files via upload

---
+++ routes/productRoute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {
+  createProduct,
+  getaProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
+  addToWishlist,
+  rating,
+  uploadImages
+} = require("../controller/productCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+router.post("/", authMiddleware, isAdmin, createProduct);
+router.put("/upload/:id", authMiddleware, isAdmin, uploadImages);
+router.get("/:id", getaProduct);
+router.put("/wishlist", authMiddleware, addToWishlist);
+router.put("/rating", authMiddleware, rating);
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
+router.get("/", getAllProduct);
+
+module.exports = router;
